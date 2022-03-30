2b995d04d1b2ea19e470d8fe6c27787d3f23fc4c
Valeria-Aguirre-Uzquiano <valeria.aguirre.uzquiano@gmail.com>
2022-03-30 16:24:10 -0400

    login form layout, validation and update routing

--- frontend/src/app/app-routing.module.ts
+++ frontend/src/app/app-routing.module.ts
@@ -5,6 +5,7 @@ import { InventoryComponent } from './components/inventory/inventory.component';
 import { SigninComponent } from './components/signin/signin.component';
 import { MovieDetailComponent } from './components/movie-detail/movie-detail.component';
 import { LoginComponent } from './components/login/login.component';
+import { CardComponent } from './components/card/card.component';
 
 const routes: Routes = [
   { path: '', redirectTo: '/movies', pathMatch: 'full' },
@@ -12,7 +13,8 @@ const routes: Routes = [
   { path: 'movies', component: ListingComponent },
   { path: 'signin', component: SigninComponent },
   { path: 'login', component: LoginComponent },
-  { path: 'movie-detail/:movie_id', component: MovieDetailComponent}
+  { path: 'movie-detail/:movie_id', component: MovieDetailComponent},
+  { path: 'card', component: CardComponent }
 ];
 
 @NgModule({
@@ -20,4 +22,4 @@ const routes: Routes = [
   exports: [RouterModule]
 })
 export class AppRoutingModule { }
-export const routingComponents = [InventoryComponent, ListingComponent, SigninComponent, LoginComponent, MovieDetailComponent];
+export const routingComponents = [InventoryComponent, ListingComponent, SigninComponent, LoginComponent, MovieDetailComponent, CardComponent];
--- frontend/src/app/app.module.ts
+++ frontend/src/app/app.module.ts
@@ -9,6 +9,7 @@ import { SigninComponent } from './components/signin/signin.component';
 import { MovieDetailComponent } from './components/movie-detail/movie-detail.component';
 import { ReactiveFormsModule } from '@angular/forms';
 import { LoginComponent } from './components/login/login.component';
+import { CardComponent } from './components/card/card.component';
 
 
 @NgModule({
@@ -18,7 +19,8 @@ import { LoginComponent } from './components/login/login.component';
     ListingComponent,
     SigninComponent,
     MovieDetailComponent,
-    LoginComponent
+    LoginComponent,
+    CardComponent
   ],
   imports: [
     BrowserModule,
