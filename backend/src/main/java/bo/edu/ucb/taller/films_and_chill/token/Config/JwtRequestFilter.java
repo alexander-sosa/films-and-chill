9d1cd59ae31c939bdb9095441176c633c956231c
alexander-sosa <junior.sosa@ucb.edu.bo>
2022-04-12 18:00:39 -0400

    added authorization headers to every needing endpoint. added logout button

--- backend/src/main/java/bo/edu/ucb/taller/films_and_chill/token/Config/JwtRequestFilter.java
+++ backend/src/main/java/bo/edu/ucb/taller/films_and_chill/token/Config/JwtRequestFilter.java
@@ -34,7 +34,7 @@ public class JwtRequestFilter extends OncePerRequestFilter{
             throws ServletException, IOException {
 
         final String requestTokenHeader = request.getHeader("Authorization");
-        System.out.println(requestTokenHeader);
+        //System.out.println(requestTokenHeader);
 
         String username = null;
         String jwtToken = null;
