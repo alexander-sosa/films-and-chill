b912b56413d5158f62b306d547525b30153e182f
alexander-sosa <junior.sosa@ucb.edu.bo>
2022-04-13 15:00:10 -0400

    authorization headers set when needed

--- frontend/src/app/services/cart.service.ts
+++ frontend/src/app/services/cart.service.ts
@@ -11,27 +11,25 @@ export class CartService {
   API_URL = 'http://localhost:8080';
   tkn = 'Bearer ' + localStorage.getItem('token');
   
-  headers = new HttpHeaders()
+  auth_headers = new HttpHeaders()
   .set('Access-Control-Allow-Origin', '*')
   .set('Authorization', this.tkn);
   
   constructor(private http:HttpClient) { }
 
   listCart(){
-    console.log("Attempting auth: ")
-    console.log(this.headers)
-    return this.http.get(this.API_URL + '/cart', {headers: this.headers});
+    return this.http.get(this.API_URL + '/cart', {headers: this.auth_headers});
   }
 
   addToCart(movie: Movie | undefined){
-    return this.http.post<HttpResponse<any>>(this.API_URL + '/cart', movie, {headers: this.headers});
+    return this.http.post<HttpResponse<any>>(this.API_URL + '/cart', movie, {headers: this.auth_headers});
   }
 
   removeFromCart(movie_id: number | undefined){
-    return this.http.delete(this.API_URL + '/cart/'+ movie_id, {headers: this.headers});
+    return this.http.delete(this.API_URL + '/cart/'+ movie_id, {headers: this.auth_headers});
   }
 
   removeAll(){
-    return this.http.delete(this.API_URL + '/cart', {headers: this.headers});
+    return this.http.delete(this.API_URL + '/cart', {headers: this.auth_headers});
   }
 }
