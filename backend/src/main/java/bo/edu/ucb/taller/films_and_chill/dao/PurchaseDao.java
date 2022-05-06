e72d6a56d3511508dccdfbc5da22b526561edc7d
PepinilloPB <edgar.carrasco@ucb.edu.bo>
2022-05-06 17:50:22 -0400

    Datos user

--- backend/src/main/java/bo/edu/ucb/taller/films_and_chill/dao/PurchaseDao.java
+++ backend/src/main/java/bo/edu/ucb/taller/films_and_chill/dao/PurchaseDao.java
@@ -1,12 +1,8 @@
 package bo.edu.ucb.taller.films_and_chill.dao;
 
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.jpa.repository.Query;
-
-import java.util.List;
 import java.util.Optional;
 
-import org.springframework.data.domain.Page;
 import org.springframework.stereotype.Repository;
 
 import bo.edu.ucb.taller.films_and_chill.dto.Purchase;
