d0e84404fe54c9d5c31a13e3c25cc136afd88935
alexander-sosa <junior.sosa@ucb.edu.bo>
2022-05-11 16:17:54 -0400

    added popularity queries

--- db/query.sql
+++ db/query.sql
@@ -1,6 +1,7 @@
 -- WARNING!
 -- Do not execute this script. Made for sample purposes only
 
+
 -- insert a movie with all data provided
 INSERT INTO movie (title, description, release_year, cost, stock, rating_id, genre_id, image_link) VALUES
 ('Rogue One: Una Historia de Star Wars', 'La batalla de la rebelión por conseguir los planos de la Estrella de la Muerte', 2016, 16.9, 10, 2, 1, '*image_link*')
@@ -25,3 +26,17 @@ lastname = 11.5,
 email = 2,
 pass = 'new hashed pass'
 WHERE user_id = 5;
+
+-- number of movie purchases
+SELECT count(*), movieid  
+FROM moviepurchase 
+GROUP BY movieid 
+ORDER BY count(*) DESC
+LIMIT 12;
+
+-- number of movie stock purchases
+SELECT sum(quantity), movieid  
+FROM moviepurchase 
+GROUP BY movieid 
+ORDER BY sum(quantity) DESC
+LIMIT 12;
